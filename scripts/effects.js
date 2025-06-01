352560cf13a64fd1d788e467ea19299432765593
MikaYBT <mystrangelife808@gmail.com>
2025-06-01 14:53:30 +0300

    .

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,22 +1,22 @@
-const gradientStatic = document.querySelector(".gradient--static");
-const gradientTracking = document.querySelector(".gradient--tracking");
+const gradientLayer1 = document.querySelector(".gradient--1");
+const gradientLayer2 = document.querySelector(".gradient--2");
 const backgroundImage = document.querySelector(".background");
 const backgroundCharacter = document.querySelector(".character");
-const gradientCursor = document.querySelector(".gradient--cursor");
-layers = [{ element: gradientTracking, factor: 0.9, x: 0, y: 0 },
-{ element: backgroundImage, factor: 0.05, x: 0, y: 0 },
+const Cursor = document.querySelector(".cursor");
+layers = [{ element: backgroundImage, factor: 0.05, x: 0, y: 0 },
 { element: backgroundCharacter, factor: 0.1, x: 0, y: 0 }];
 
 function gradientMovement() {
-    gradientStatic.style.backgroundPosition = `${100 * Math.random()}% ${100 * Math.random()}%`;
-    gradientStatic.style.backgroundSize = `${200 + 100 * Math.random()}% ${200 + 100 * Math.random()}%`;
-    gradientTracking.style.backgroundSize = `${200 + 100 * Math.random()}% ${200 + 100 * Math.random()}%`;
+    gradientLayer1.style.backgroundPosition = `${100 * Math.random()}% ${100 * Math.random()}%`;
+    gradientLayer1.style.backgroundSize = `${200 + 100 * Math.random()}% ${200 + 100 * Math.random()}%`;
+    gradientLayer2.style.backgroundPosition = `${100 * Math.random()}% ${100 * Math.random()}%`;
+    gradientLayer2.style.backgroundSize = `${200 + 100 * Math.random()}% ${200 + 100 * Math.random()}%`;
     setTimeout(gradientMovement, 5000);
 }
 
 function gradientOpacity() {
-    const randomOpacity = Math.random();
-    gradientStatic.style.opacity = `${randomOpacity}`;
+    gradientLayer1.style.opacity = `${Math.random}`;
+    gradientLayer2.style.opacity = `${Math.random}`;
     setTimeout(gradientOpacity, 5000);
 }
 
@@ -26,8 +26,8 @@ function lerp(start, end, factor) {
 
 gradientOpacity();
 gradientMovement();
-
-const lerpFactor = 0.005;
+let moveTimeout;
+const lerpFactor = 0.5;
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 
@@ -36,41 +36,38 @@ let centreTimeout;
 document.addEventListener('mousemove', (e) => {
     mouseX = e.clientX;
     mouseY = e.clientY;
-    gradientCursor.style.transform = `translate(${mouseX - window.innerWidth/ 2 - gradientCursor.clientWidth/2}px, ${mouseY - window.innerHeight/ 2 - gradientCursor.clientHeight/2}px)`;
-    mouseMoving = true;
-    clearTimeout(centreTimeout);
-    centreTimeout = setTimeout(() => { mouseMoving = false; }, 2000);
-});
-
-document.querySelector(".menu").addEventListener("mouseenter",  (e) => {
-    gradientCursor.style.backgroundSize = "200% 200%"
-})
-document.querySelector(".menu").addEventListener("mouseleave",  (e) => {
-    gradientCursor.style.backgroundSize = "100% 100%"
-})
-
-
-function backgroundParallax() {
+    Cursor.style.top = `${mouseY}px`;
+    Cursor.style.left = `${mouseX}px`;
     let centerX = window.innerWidth / 2;
     let centerY = window.innerHeight / 2;
     let adaptiveMoving = (1920 - window.innerWidth) / 10;
     layers.forEach(layer => {
-        if (mouseMoving) {
-            const targetX = (centerX - mouseX) * layer.factor;
-            const targetY = (centerY - mouseY) * layer.factor;
-            layer.x = lerp(layer.x, targetX, lerpFactor);
-            layer.y = lerp(layer.y, targetY, lerpFactor);
-            layer.element.style.backgroundPosition = `calc(50% + ${layer.x}px - ${adaptiveMoving}px) calc(50% + ${layer.y}px)`;
-        }
-        else {
-            layer.x = lerp(layer.x, 0, lerpFactor);
-            layer.y = lerp(layer.y, 0, lerpFactor);
-            layer.element.style.backgroundPosition = `calc(50% + ${layer.x}px - ${adaptiveMoving}px) calc(50% + ${layer.y}px)`;
-        }
+        const targetX = (centerX - mouseX) * layer.factor;
+        const targetY = (centerY - mouseY) * layer.factor;
+        layer.x = lerp(layer.x, targetX, lerpFactor);
+        layer.y = lerp(layer.y, targetY, lerpFactor);
+        layer.element.style.backgroundPosition = `calc(50% + ${layer.x}px - ${adaptiveMoving}px) calc(50% + ${layer.y}px)`;
     });
-    requestAnimationFrame(backgroundParallax);
-}
-backgroundParallax();
+
+});
+
+document.addEventListener("mouseenter", () => {
+    Cursor.style.opacity = "0.4";
+})
+document.addEventListener("mouseleave", () => {
+    Cursor.style.opacity = "0";
+})
+
+document.querySelectorAll("li").forEach(button => button.addEventListener("mouseenter", () => {
+    Cursor.style.width = "300px";
+    Cursor.style.height = "100px";
+}))
+
+document.querySelectorAll("li").forEach(button => button.addEventListener("mouseleave", () => {
+    Cursor.style.width = "300px";
+    Cursor.style.height = "300px";
+}))
+
 
 window.addEventListener('resize', () => {
     mouseX = window.innerWidth / 2;
