feead9551ed8c43c3ef022c024fffab4ef2c890e
MikaYBT <mystrangelife808@gmail.com>
2025-06-01 16:27:00 +0300

    .

--- scripts/effects.js
+++ scripts/effects.js
@@ -6,11 +6,21 @@ const Cursor = document.querySelector(".cursor");
 layers = [{ element: backgroundImage, factor: 0.05, x: 0, y: 0 },
 { element: backgroundCharacter, factor: 0.1, x: 0, y: 0 }];
 
-window.mobileCheck = function () {
-    let check = false;
-    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-    return check;
-};
+function detectDeviceType() {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobileUA = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+    const isTouchDevice = 'ontouchstart' in window ||
+        navigator.maxTouchPoints > 0 ||
+        navigator.msMaxTouchPoints > 0;
+    const isSmallScreen = window.innerWidth < 768 ||
+        (window.innerWidth < 1024 && window.innerHeight > window.innerWidth);
+    const isMobile = isMobileUA && (isTouchDevice || isSmallScreen);
+    return {
+        isMobile
+    };
+}
+
+const mobileCheck = detectDeviceType();
 
 //gradient animation
 
@@ -56,32 +66,26 @@ function requestGyroPermission() {
 }
 
 function startGyro() {
-    window.addEventListener('deviceorientation', handleOrientation);
+    window.addEventListener('deviceorientation', GyroParallax);
     isGyroActive = true;
 }
 
-function handleOrientation(event) {
+function GyroParallax(event) {
     const beta = event.beta;
     const gamma = event.gamma;
     const alpha = event.alpha;
-    const smoothedBeta = lastBeta + (beta - lastBeta) * 0.2;
-    const smoothedGamma = lastGamma + (gamma - lastGamma) * 0.2;
-    lastBeta = smoothedBeta;
-    lastGamma = smoothedGamma;
-    GyroParallax(smoothedBeta, smoothedGamma);
-}
-
-function GyroParallax(beta, gamma) {
-    const maxTilt = 50;
-    const limitedBeta = Math.max(Math.min(beta, maxTilt), -maxTilt);
-    const limitedGamma = Math.max(Math.min(gamma, maxTilt), -maxTilt);
-    const offsetX = limitedGamma * 1.5;
-    const offsetY = limitedBeta * 1.5;
-    let adaptiveMoving = (1920 - window.innerWidth) / 10;
-    layers.forEach(layer => {
+    layer.forEach(layer => {
+        const smoothedBeta = lastBeta + (beta - lastBeta) * layer.factor;
+        const smoothedGamma = lastGamma + (gamma - lastGamma) * layer.factor;
+        const maxTilt = 50;
+        lastBeta = smoothedBeta;
+        lastGamma = smoothedGamma;
+        const offsetX = Math.max(Math.min(smoothedBeta, maxTilt), -maxTilt);
+        const offsetY = Math.max(Math.min(smoothedGamma, maxTilt), -maxTilt);
+        let adaptiveMoving = (1920 - window.innerWidth) / 10;
         layer.element.style.backgroundPosition = `calc(50% + ${offsetX}px - ${adaptiveMoving}px) calc(50% + ${offsetY}px)`;
     });
-}
+};
 
 window.addEventListener('DOMContentLoaded', initGyroParallax);
 
@@ -100,7 +104,7 @@ let mouseMoving = false;
 let centreTimeout;
 
 document.addEventListener('mousemove', (e) => {
-    if (window.mobileCheck) {
+    if (!mobileCheck.isMobile) {
         mouseX = e.clientX;
         mouseY = e.clientY;
         Cursor.style.top = `${mouseY}px`;
@@ -123,18 +127,18 @@ document.addEventListener('mousemove', (e) => {
 //cursor
 
 document.addEventListener("mouseenter", () => {
-    if (window.mobileCheck) {
+    if (!mobileCheck.isMobile) {
         Cursor.style.opacity = "0.4";
     }
 
 })
 document.addEventListener("mouseleave", () => {
-    if (window.mobileCheck) {
+    if (!mobileCheck.isMobile) {
         Cursor.style.opacity = "0";
     }
 })
 document.addEventListener("mousemove", () => {
-    if (window.mobileCheck) {
+    if (!mobileCheck.isMobile) {
         Cursor.style.opacity = "0.4";
     }
 })
