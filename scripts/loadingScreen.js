1ae003b692afd5ee941a09dc65173c8c88907f8e
MikaYBT <mystrangelife808@gmail.com>
2025-06-01 18:01:24 +0300

    .

--- scripts/loadingScreen.js
+++ scripts/loadingScreen.js
@@ -1,7 +1,7 @@
 //loading screen
 const loadingScreen = document.querySelector(".loading");
-window.addEventListener("DOMContentLoaded", () => {
-    loadingScreen.style.opacity= "0";
+window.addEventListener("load", () => {
+    loadingScreen.style.opacity = "0";
     setTimeout(() => {
         loadingScreen.style.display = "none";
     }, 1000);
